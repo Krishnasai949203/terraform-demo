feae3b7b065983fad955c89cb6930a790a6c80fd
Krishnasai949203 <158172453+Krishnasai949203@users.noreply.github.com>
2024-07-23 22:01:46 +0530

    Create variable.tf

---
+++ python_app/variable.tf
@@ -0,0 +1,20 @@
+variable "cidr" {
+    type = string
+    description = "cidr of vpc"
+  
+}
+variable "instance_type" {
+    type = string
+    description = "value for instance_type"
+  
+}
+variable "cidr_pub" {
+    type = string
+    description = "cidr of vpc_pub"
+  
+}
+variable "ami" {
+    type = string
+    description = "value for ami"
+  
+}
