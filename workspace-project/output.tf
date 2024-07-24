6bb172de748b10ffbdfe760b2b9bb6918cd195a0
Krishnasai949203 <158172453+Krishnasai949203@users.noreply.github.com>
2024-07-24 23:46:43 +0530

    Create output.tf

---
+++ workspace-project/output.tf
@@ -0,0 +1,24 @@
+output "instance_id" {
+    value = module.aws_instance.instance_id
+  
+}
+output "instance_public_ip" {
+    value = module.aws_instance.instance_public_ip
+  
+}
+output "vpc_id" {
+    value = module.aws_instance.vpc_id
+  
+}
+output "subnet_id" {
+    value = module.aws_instance.subnet_id
+  
+}
+output "igw_id" {
+    value = module.aws_instance.igw_id
+  
+}
+output "route_table_id" {
+    value = module.aws_instance.route_table_id
+  
+}
