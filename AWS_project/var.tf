bb5bc1ed2148b3bfec603239f6a093aabf0397bd
Krishnasai949203 <158172453+Krishnasai949203@users.noreply.github.com>
2024-07-26 23:15:20 +0530

    Create var.tf

---
+++ AWS_project/var.tf
@@ -0,0 +1,20 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+
+}
+variable "cidr_pub1" {
+  default = "10.0.1.0/24"
+
+}
+variable "cidr_pub2" {
+  default = "10.0.2.0/24"
+
+}
+variable "ami" {
+  default = "ami-0583d8c7a9c35822c"
+
+}
+variable "instance_type" {
+  default = "t2.micro"
+
+}
