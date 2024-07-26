7e18ebe63a10eca1530dcc55bd63972d79cc2d2c
Krishnasai949203 <158172453+Krishnasai949203@users.noreply.github.com>
2024-07-26 23:13:30 +0530

    Create lb.tf

---
+++ AWS_project/lb.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "lb http sg rules"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
+resource "aws_lb" "test" {
+  name               = "ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [aws_subnet.pub1.id, aws_subnet.pub2.id]
+
+  enable_deletion_protection = true
+
+
+  tags = {
+    Name = "ALB"
+  }
+}
+resource "aws_lb_target_group" "tg" {
+  name        = "tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+
+}
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+
+}
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
